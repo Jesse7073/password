12d9bcdb2f056be96513ce7f8761b7b2d8dc73ac
Jesse Meng <j2093j@gmail.com>
2021-09-05 13:37:53 +0800

    password_block

--- password.py
+++ password.py
@@ -1,10 +1,13 @@
 x = 3
 password = 'a123456'
 while x > 0:
+    x = x - 1
     enter = input('請輸入密碼:')
-    if password != enter:
-        x = x - 1
+    if password == enter:
+        print('登入成功')
+        break
+    elif password != enter and x > 0:
         print('密碼錯誤!還有', x, '次機會')
     else:
-        print('登入成功')
+        print('帳號已被封鎖')
         break
